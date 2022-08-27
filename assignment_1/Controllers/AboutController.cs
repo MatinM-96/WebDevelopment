69057f8078638c925535352e9cf70b611c1a0296
Matin Mohammadi <matinm@student.uia.no>
2022-08-27 14:45:15 +0200

    assignment_1

---
+++ assignment_1/Controllers/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace assignment_1.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        } 
+        
+    }
+}
