5faa029df7527b3502cbb9e24ebf56c1847d804c
Ali Shahab Rezaii <alisr18@student.uia.no>
2022-12-08 08:35:34 +0100

    Rentee history

--- PrivatePark/Data/ApplicationDbInitializer.cs
+++ PrivatePark/Data/ApplicationDbInitializer.cs
@@ -39,7 +39,22 @@ public class ApplicationDbInitializer
                 DateOfBirth = new DateTime(1996, 08, 12)
             }
         };
+
+        var user2 = new ApplicationUser()
+        {
+            Firstname = "Test2",
+            Lastname = "User2",
+            UserName = "user2@uia.no",
+            Nickname = "User2",
+            Email = "user2@uia.no",
+            EmailConfirmed = true,
+            DateOfBirth = new DateTime(1996, 08, 12)
+        };       
+        
         um.CreateAsync(user[0], "Password1.").Wait();
+        await um.AddToRoleAsync(user[0], "Both");
+        
+        um.CreateAsync(user2, "Password1.").Wait();
         await um.AddToRoleAsync(user[0], "Both");
 
 
@@ -61,7 +76,7 @@ public class ApplicationDbInitializer
         
         //city  
         string city = "Grimstad";
-        const string zicode = "4879 ";
+        const string zicode = "4879";
         string street = "jon lilletuns vei 2A";
         float price = 20;
 
@@ -71,17 +86,24 @@ public class ApplicationDbInitializer
         string street2 = "Jon Lilletuns vei 9";
         float price2 = 15;
         
+        string city3 = "Grimstad";
+        const string zicode3 = "4879";
+        string street3 = "Tønnevoldsgate 26";
+        float price3 = 10;
+        
 
         var address = new[]
         {
             new Address(street, city, zicode, price, false, "Car", 1),
             new Address(street2,city2,zicode2, price2, true, "Motorcycle", 2),
+            new Address(street3,city3,zicode3, price3, true, "Motorcycle", 2)
         };
         await db.Addresses.AddRangeAsync(address);
 
 
         address[0].User = admin;
         address[1].User = user[0];
+        address[2].User = user2;
         
         
         
@@ -125,8 +147,28 @@ public class ApplicationDbInitializer
             
         }
 
+        Rootobject oRootObject3;
+        
+        Task<string> jason_taks_string3=  FindLocation.GetTheLatitudeAndLongitude(city3, street3, zicode3);
+        string jason3 = jason_taks_string3.Result;
+        
+        oRootObject3 = JsonConvert.DeserializeObject<Rootobject>(jason3);
+
+        Location loc3 = new Location(); 
+        
+        if (oRootObject3 != null)
+        {
+            loc3 = oRootObject3.results[0].Geometry.location;
+            db.Locations.AddRange(loc3);
+            //loc.Address = address[0];
+            
+        }
+        
+        
         address[0].Location = loc;
-        address[1].Location = loc2; 
+        address[1].Location = loc2;
+        address[2].Location = loc3;
+        
 
 
 
@@ -140,7 +182,8 @@ public class ApplicationDbInitializer
            new Car("AJ57222", "vareBil", user[0]),
            new Car("AJ57223", "Lastebil", user[0]),
 
-           new Car("AJ57220", "Car", admin)
+           new Car("AJ57220", "Car", admin),
+           new Car("EL1234", "Car", user2)
 
        };
        await db.Cars.AddRangeAsync(car);
@@ -160,7 +203,7 @@ public class ApplicationDbInitializer
        var park2 = new Parkering
        {
            StartTime = DateTime.Now,
-           Address = address[0],
+           Address = address[1],
            Renter = user[0],
            Rentee = admin,
            Car = car[0],
@@ -168,10 +211,35 @@ public class ApplicationDbInitializer
            TotalTime = DateTime.Now - DateTime.Now,
            PricePaid = 100
        };
+       
+       var park3 = new Parkering
+       {
+           StartTime = DateTime.Now,
+           Address = address[0],
+           Renter = admin,
+           Rentee = user[0],
+           Car = car[0],
+           EndTime = DateTime.Now,
+           TotalTime = DateTime.Now - DateTime.Now,
+           PricePaid = 200
+       };
+       
+       var park4 = new Parkering
+       {
+           StartTime = DateTime.Now,
+           Address = address[2],
+           Renter = user2,
+           Rentee = user[0],
+           Car = car[2],
+           EndTime = DateTime.Now,
+           TotalTime = DateTime.Now - DateTime.Now,
+           PricePaid = 200
+       };
 
        await db.Parkerings.AddRangeAsync(park);
        await db.Parkerings.AddRangeAsync(park2);
-      
+       await db.Parkerings.AddRangeAsync(park3);
+       await db.Parkerings.AddRangeAsync(park4);
 
        await db.SaveChangesAsync();
        
